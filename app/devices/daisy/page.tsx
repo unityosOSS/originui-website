fc7e0faf553d7aac0b823fa4e99295c7f0b83261
matik <wierzejskimateusz8@gmail.com>
2024-04-03 16:54:17 +0200

    add new device (daisy)

---
+++ app/devices/daisy/page.tsx
@@ -0,0 +1,13 @@
+import SunfishDevice from '@/components/devices/models/daisy'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "OriginUi | Daisy",
+    description: "OriginUi is a modified version LineageOS that brings some uhh... idk things",
+};
+
+export default function Sunfish() {
+    return (
+        <SunfishDevice />
+    )
+}
